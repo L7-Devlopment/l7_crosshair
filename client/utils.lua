155d1cab308077d416106afecde851572ce288e9
VII <vii.business@outlook.com>
2024-06-24 03:36:51 -0500

    update to use gfx now

--- client/utils.lua
+++
@@ -1,3 +0,0 @@
-function isPlayerAiming()
-    return (IsPlayerFreeAiming(cache.playerId) or IsControlPressed(0, 25)) and true or false
-end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,7 @@
-
 fx_version 'cerulean'
+
 game 'gta5'
+
 lua54 'yes'
 
-client_scripts { '@ox_lib/init.lua', 'config.lua', 'client/*.lua' }
-
-ui_page 'html/crosshair.html'
-
-files { 'html/crosshair.html', 'html/style/style.css', 'html/app/app.js'
-}
-
-dependency 'ox_lib'
+description 'Crosshair Script'
